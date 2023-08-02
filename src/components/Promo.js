f50fb65dd2458acdcf75b65351a6dfc4b216aa38
Ivan <vanyok.korobka@ukr.net>
2023-08-02 15:45:42 +0300

    added adaptive to several elem in promo section

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -2,9 +2,10 @@
 
 function Promo () {
   return (
-    <section className="px-[15px] mx-[9.6px] pt-[60]">
+    <section className="px-[15px] mx-[9.6px] pt-[78px]">
+      <div className="absolute top-24 left-[50%] translate-x-[-50%] hidden lg:block lg:text-[150px] xl:text-[180px] 2xl:text-[210px] font-bold leading-normal tracking-[-16.8px] uppercase opacity-[0.07999999821186066] bg-gradient-to-b from-white to-transparent bg-clip-text text-transparent  ">Мотошкола</div>
       <div className="flex flex-col justify-center items-center">
-        <h2 className="text-[#fff] text-[70px] font-extrabold tracking-[2.8px] uppercase leading-normal">ПЕРША ПРЕМІАЛЬНА</h2>
+        <h2 className="text-[#fff] text-[56px] sm:text-[45px] md:text-[56px] lg:text-[70px] font-extrabold tracking-[2.8px] uppercase leading-normal">ПЕРША ПРЕМІАЛЬНА</h2>
         <div className="text-[40px] text-[#fff] font-medium tracking-[1.6px] uppercase leading-normal">МОТОШКОЛА УКРАЇНИ</div>
         <p className="max-w-[416px] text-[#C1C1C1] text-base font-normal text-center">Ексклюзивна колаборація від легендарних Harley Davidson та мотошколи RSMOTO. </p>
         <div className="flex gap-[24.67px] pt-[30px]">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient': 'linear-gradient(180deg, #FFF 0%, rgba(255, 255, 255, 0.00) 88.02%)',
+      },
+      backgroundClip: {
+        'text': 'text',
+      },
+      textFillColor: {
+        'transparent': 'transparent',
+      },
+    },
   },
   plugins: [],
 }
