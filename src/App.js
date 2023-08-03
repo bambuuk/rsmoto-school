9bdf1e76572a53ff37086fdffc55bb4ab9e156b1
Ivan <vanyok.korobka@ukr.net>
2023-08-04 02:39:18 +0300

    created section about instructor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import AboutStudying from "./components/AboutStudying";
 import Header from "./components/Header";
+import Instructor from "./components/Instructor";
 import Promo from "./components/Promo";
 
 
 function App() {
   return (
-    <div className="App ">
+    <div>
       <Header />
       <main>
         <Promo />
         <AboutStudying />
+        <Instructor />
       </main>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-moto': 'linear-gradient(180deg, rgba(32, 32, 32, 0.00) 0%, #202020 100%)',
+        'gradient-moto-vertical': 'linear-gradient(90deg, rgba(32, 32, 32, 0.00) 0%, #202020 100%)',
         'promo-motorcycle': "url('/src/asserts/first-bike.png')",
         'custom-lightgray': 'lightgray 50% / cover no-repeat'
       },
